683450a963785261b31826025eb8708f8f0d6a92
Brendan Mullins <b.mullins@agentur-loop.com>
2017-02-20 12:47:35 +0100

    add babel build, source file in "src" dir

---
+++ src/vue-parallax-js.js
@@ -0,0 +1,112 @@
+let parallaxjs = function (options) {
+  this.options = options
+}
+
+parallaxjs.prototype = {
+  items: [],
+  active: true,
+
+  setStyle (item, value) {
+    if (item.arg === 'centerX')
+      value += ' translateX(-50%)'
+
+    let el = item.el;
+    let prop = 'Transform';
+    el.style["webkit" + prop] = value;
+    el.style["moz" + prop] = value;
+    el.style["ms" + prop] = value;
+  },
+
+  add (el, binding) {
+    let value = binding.value
+    let arg = binding.arg
+    let style = el.currentStyle || window.getComputedStyle(el);
+
+    this.items.push({
+      el: el,
+      initialOffsetTop: el.offsetTop + el.offsetParent.offsetTop - parseInt(style.marginTop),
+      style,
+      value,
+      arg,
+      clientHeight: el.clientHeight || el.offsetHeight || el.scrollHeight,
+      count: 0
+    })
+  },
+
+  move () {
+    if (!this.active) return
+    if (window.innerWidth < this.options.minWidth || 0) {
+      this.items.map((item) => {
+        this.setStyle(item, 'translateY(' + 0 + 'px) translateZ(0px)')
+      })
+
+      return
+    }
+
+    let scrollTop = window.scrollY || window.pageYOffset
+    let windowHeight = window.innerHeight
+    let windowWidth = window.innerWidth
+
+    this.items.map((item) => {
+        let pos = (scrollTop + windowHeight)
+        let elH = item.clientHeight
+        if (item.count > 50) {
+          item.count = 0;
+          elH = item.el.clientHeight || item.el.offsetHeight || item.el.scrollHeight
+        }
+
+
+        pos = pos - (elH / 2)
+        pos = pos - (windowHeight / 2)
+        pos = pos * item.value
+
+        let offset = item.initialOffsetTop
+        offset = offset * -1
+        offset = offset * item.value
+        pos = pos + offset
+
+        pos = pos.toFixed(2)
+
+        item.count++
+        this.setStyle(item, 'translateY(' + pos + 'px)')
+    })
+  }
+}
+
+export default {
+  install (Vue, options = {}) {
+    var p = new parallaxjs(options)
+
+    window.addEventListener('scroll', () => {
+      requestAnimationFrame(() => {
+        p.move(p)
+      })
+    }, {passive: true})
+    window.addEventListener('resize', () => {
+      requestAnimationFrame(() => {
+        p.move(p)
+      })
+    }, {passive: true})
+
+    Vue.prototype.$parallaxjs = p
+    window.$parallaxjs = p
+    Vue.directive('parallax', {
+      bind (el, binding) {
+      },
+      inserted (el, binding) {
+        p.add(el, binding)
+        p.move(p)
+      },
+      // unbind(el, binding) {
+      //   p.remove(el)
+      // }
+      // bind: parallaxjs.add(parallaxjs),
+      // update(value) {
+      //  parallaxjs.update(value)
+      // },
+      // update(el, binding) {
+      //   console.log("cup");
+      // },
+    })
+  }
+}
--- vue-parallax-js.js
+++ vue-parallax-js.js
@@ -1,114 +1,101 @@
-let parallaxjs = function (options) {
-  this.options = options
-}
+var parallaxjs = function parallaxjs(options) {
+  this.options = options;
+};
 
 parallaxjs.prototype = {
   items: [],
   active: true,
 
-  setStyle (item, value) {
-    if (item.arg === 'centerX')
-      value += ' translateX(-50%)'
+  setStyle: function setStyle(item, value) {
+    if (item.arg === 'centerX') value += ' translateX(-50%)';
 
-    let el = item.el;
-    let prop = 'Transform';
+    var el = item.el;
+    var prop = 'Transform';
     el.style["webkit" + prop] = value;
     el.style["moz" + prop] = value;
     el.style["ms" + prop] = value;
   },
-
-  add (el, binding) {
-    let value = binding.value
-    let arg = binding.arg
-    let style = el.currentStyle || window.getComputedStyle(el);
-
-    // console.log(;
+  add: function add(el, binding) {
+    var value = binding.value;
+    var arg = binding.arg;
+    var style = el.currentStyle || window.getComputedStyle(el);
 
     this.items.push({
       el: el,
       initialOffsetTop: el.offsetTop + el.offsetParent.offsetTop - parseInt(style.marginTop),
-      style,
-      value,
-      arg,
+      style: style,
+      value: value,
+      arg: arg,
       clientHeight: el.clientHeight || el.offsetHeight || el.scrollHeight,
       count: 0
-    })
+    });
   },
+  move: function move() {
+    var _this = this;
 
-  move () {
-    if (!this.active) return
+    if (!this.active) return;
     if (window.innerWidth < this.options.minWidth || 0) {
-      this.items.map((item) => {
-        this.setStyle(item, 'translateY(' + 0 + 'px) translateZ(0px)')
-      })
+      this.items.map(function (item) {
+        _this.setStyle(item, 'translateY(' + 0 + 'px) translateZ(0px)');
+      });
 
-      return
+      return;
     }
 
-    let scrollTop = window.scrollY || window.pageYOffset
-    let windowHeight = window.innerHeight
-    let windowWidth = window.innerWidth
+    var scrollTop = window.scrollY || window.pageYOffset;
+    var windowHeight = window.innerHeight;
+    var windowWidth = window.innerWidth;
 
-    this.items.map((item) => {
-        let pos = (scrollTop + windowHeight)
-        let elH = item.clientHeight
-        if (item.count > 50) {
-          item.count = 0;
-          elH = item.el.clientHeight || item.el.offsetHeight || item.el.scrollHeight
-        }
+    this.items.map(function (item) {
+      var pos = scrollTop + windowHeight;
+      var elH = item.clientHeight;
+      if (item.count > 50) {
+        item.count = 0;
+        elH = item.el.clientHeight || item.el.offsetHeight || item.el.scrollHeight;
+      }
 
+      pos = pos - elH / 2;
+      pos = pos - windowHeight / 2;
+      pos = pos * item.value;
 
-        pos = pos - (elH / 2)
-        pos = pos - (windowHeight / 2)
-        pos = pos * item.value
+      var offset = item.initialOffsetTop;
+      offset = offset * -1;
+      offset = offset * item.value;
+      pos = pos + offset;
 
-        let offset = item.initialOffsetTop
-        offset = offset * -1
-        offset = offset * item.value
-        pos = pos + offset
+      pos = pos.toFixed(2);
 
-        pos = pos.toFixed(2)
-
-        item.count++
-        this.setStyle(item, 'translateY(' + pos + 'px)')
-    })
+      item.count++;
+      _this.setStyle(item, 'translateY(' + pos + 'px)');
+    });
   }
-}
+};
 
 export default {
-  install (Vue, options = {}) {
-    var p = new parallaxjs(options)
+  install: function install(Vue) {
+    var options = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-    window.addEventListener('scroll', () => {
-      requestAnimationFrame(() => {
-        p.move(p)
-      })
-    }, {passive: true})
-    window.addEventListener('resize', () => {
-      requestAnimationFrame(() => {
-        p.move(p)
-      })
-    }, {passive: true})
+    var p = new parallaxjs(options);
 
-    Vue.prototype.$parallaxjs = p
-    window.$parallaxjs = p
+    window.addEventListener('scroll', function () {
+      requestAnimationFrame(function () {
+        p.move(p);
+      });
+    }, { passive: true });
+    window.addEventListener('resize', function () {
+      requestAnimationFrame(function () {
+        p.move(p);
+      });
+    }, { passive: true });
+
+    Vue.prototype.$parallaxjs = p;
+    window.$parallaxjs = p;
     Vue.directive('parallax', {
-      bind (el, binding) {
-      },
-      inserted (el, binding) {
-        p.add(el, binding)
-        p.move(p)
-      },
-      // unbind(el, binding) {
-      //   p.remove(el)
-      // }
-      // bind: parallaxjs.add(parallaxjs),
-      // update(value) {
-      //  parallaxjs.update(value)
-      // },
-      // update(el, binding) {
-      //   console.log("cup");
-      // },
-    })
+      bind: function bind(el, binding) {},
+      inserted: function inserted(el, binding) {
+        p.add(el, binding);
+        p.move(p);
+      }
+    });
   }
-}
+};
