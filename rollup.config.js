acd2877c13263de1a6f873621dfffc413f45caed
qwe <megamaaaan@gmail.com>
2017-11-29 13:38:38 +0100

    Build with rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import babel from 'rollup-plugin-babel'
+import builtins from 'rollup-plugin-node-builtins'
+import commonjs from 'rollup-plugin-commonjs'
+import resolve from 'rollup-plugin-node-resolve'
+import flow from 'rollup-plugin-flow'
+import uglify from 'rollup-plugin-uglify'
+import { minify } from 'uglify-es'
+
+export default {
+  input: 'src/vue-parallax-js.js',
+  output: [
+    {
+      file: 'lib/vue-parallax-js.es.js',
+      format: 'es'
+    },
+    {
+      file: 'lib/vue-parallax-js.cjs.js',
+      format: 'cjs'
+    },
+    {
+      file: 'lib/vue-parallax-js.js',
+      format: 'iife',
+      name: 'ParallaxJS'
+    }
+  ],
+  plugins: [builtins(), resolve(), flow(), commonjs(), babel(), uglify({}, minify)],
+  external: ['postcss', 'postcss-selector-parser', 'fs'],
+  sourceMap: false
+}
